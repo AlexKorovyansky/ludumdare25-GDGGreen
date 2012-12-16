055230b610b260446a9e96722e4829359a8dfd35
Alex Korovyansky <korovyansk@gmail.com>
2012-12-16 17:29:20 +0700

    remove shtori, decrease lifetimes

--- game/src/Resource.js
+++ game/src/Resource.js
@@ -8,42 +8,35 @@ var pee_config = [
 		after:"game/res/armchair_after.png",
 		x: 300,
 		y: 240,
-		level: 3
+		level: 1
 	},
 	{
 		before:"game/res/tv_before.png",
 		after:"game/res/tv_after.png",
 		x: 120,
 		y: 240,
-		level: 5
+		level: 3
 	},
 	{
 		before:"game/res/fish_before.png",
 		after:"game/res/fish_after.png",
 		x: 600,
 		y: 140,
-		level: 2
-	},
-	{
-		before:"game/res/curtains_before.png",
-		after:"game/res/curtains_after.png",
-		x: 166,
-		y: 540,
-		level: 3
+		level: 1
 	},
 	{
 		before:"game/res/flower_before.png",
 		after:"game/res/flower_after.png",
 		x: 350,
 		y: 470,
-		level: 2
+		level: 1
 	},
 	{
 		before:"game/res/boots_before.png",
 		after:"game/res/boots_after.png",
 		x: 740,
 		y: 370,
-		level: 1
+		level: 0.5
 	},
 ];
 
