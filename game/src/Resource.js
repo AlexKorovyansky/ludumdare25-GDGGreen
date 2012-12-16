0aabf65870380c1887bd9ae27139a75973f24052
Alex Korovyansky <korovyansk@gmail.com>
2012-12-16 15:33:57 +0700

    descreased lifetimes

--- game/src/Resource.js
+++ game/src/Resource.js
@@ -7,42 +7,42 @@ var pee_config = [
 		after:"game/res/armchair_after.png",
 		x: 300,
 		y: 240,
-		level: 5
+		level: 3
 	},
 	{
 		before:"game/res/tv_before.png",
 		after:"game/res/tv_after.png",
 		x: 120,
 		y: 240,
-		level: 10
+		level: 5
 	},
 	{
 		before:"game/res/fish_before.png",
 		after:"game/res/fish_after.png",
 		x: 600,
 		y: 140,
-		level: 3
+		level: 2
 	},
 	{
 		before:"game/res/curtains_before.png",
 		after:"game/res/curtains_after.png",
 		x: 166,
 		y: 540,
-		level: 5
+		level: 3
 	},
 	{
 		before:"game/res/flower_before.png",
 		after:"game/res/flower_after.png",
 		x: 350,
 		y: 470,
-		level: 3
+		level: 2
 	},
 	{
 		before:"game/res/boots_before.png",
 		after:"game/res/boots_after.png",
 		x: 740,
 		y: 370,
-		level: 2
+		level: 1
 	},
 ];
 
