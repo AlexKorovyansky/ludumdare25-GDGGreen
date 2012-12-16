06ff0a3543c3d088a9ca8971c10a0450ab67b1ed
Andrey Krivko <jastkand@gmail.com>
2012-12-16 15:36:48 +0700

    fliping

---
+++ game/src/AngerBar.js
@@ -0,0 +1,32 @@
+var AngerBar = cc.Sprite.extend({
+    _radians:0,
+    _speed:0,
+    _catched:false,
+    ctor:function () {
+        this._super();
+        this._speed = 600;
+        this.initWithFile('game/res/cat_right.png');
+    },
+    update:function(dt) {
+      this.setRotation(this._radians);
+
+      if (this._catched){
+        this._stateProgress -= 50 * dt;
+      }
+      if (this._state && !this._disabled && this._stateProgress <= 0) {
+        this.initWithFile(this.fileNameActive);
+        this._state = false;
+        this._disabled = true;
+      }
+    },
+    setChatched:function(){
+      this._catched = true;
+    },
+    handleTouch:function(touchLocation)
+    {
+      this.stopAllActions();
+      var time = uu.timeMP(this.getPosition(), this._speed, touchLocation);
+      console.log(this._speed + " " + time);
+      this.runAction( cc.MoveTo.create(time, touchLocation) );
+    }
+});
